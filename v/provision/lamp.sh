6c7e297429469f738f171dc9ce4d8f588af28350
Muhammad Fauzil Haqqi <me@haqqi.net>
2016-11-30 19:27:46 +0700

    Create shell script for apache2 and php5

---
+++ v/provision/lamp.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# change mirror to singapore
+if ! [ -f /etc/apt/sources.list.origin ]; then
+  sudo mv /etc/apt/sources.list /etc/apt/source.list.origin
+  sudo cp /vagrant/v/config/sources.list /etc/apt/sources.list
+fi
+
+# update the package
+sudo apt-get update
+
+# install apache2
+sudo apt-get install -y apache2
+
+# create symbolic link
+if ! [ -L /var/www ]; then
+  sudo rm -rf /var/www
+  sudo ln -fs /vagrant /var/www
+fi
