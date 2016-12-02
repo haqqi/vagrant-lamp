43b31cd2586bac6122c466c6bf69830241a30f5f
Muhammad Fauzil Haqqi <me@haqqi.net>
2016-12-02 14:57:09 +0700

    Install nodejs and bower

--- _setup/provision/03-development.sh
+++ _setup/provision/03-development.sh
@@ -12,9 +12,16 @@ sudo wget http://www.webmin.com/jcameron-key.asc
 sudo apt-key add jcameron-key.asc
 
 sudo apt-get update -qq
-sudo apt-get install -y webmin
+sudo apt-get install -y webmin > /vagrant/setup.log 2>&1
 
 echo -e "\n--- Disable SSL for webmin development ---\n"
 sudo sed -i "s/ssl=1/ssl=0/g" /etc/webmin/miniserv.conf
 sudo service webmin restart
 echo -e "\n--- Access webmin in your server via port 10000 ---\n"
+
+
+echo -e "Install nodejs 6 & bower"
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install -y nodejs > /vagrant/setup.log 2>&1
+npm install -g bower
+
