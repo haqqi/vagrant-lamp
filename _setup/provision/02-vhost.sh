3cd3b6d8834de19ef4e288e8550b46e7b6d18e0d
Muhammad Fauzil Haqqi <me@haqqi.net>
2016-12-02 14:56:51 +0700

    Finish vhost setup shell

--- _setup/provision/02-vhost.sh
+++ _setup/provision/02-vhost.sh
@@ -2,11 +2,22 @@
 
 # Setup vhost development
 
+APACHE_SITE=/etc/apache2/sites-available
 
-# Remove all vhost in sites-available
-
+# Clearing up current configurations
+sudo rm /etc/apache2/sites-enabled/*.conf
+sudo rm $APACHE_SITE/*.conf
 
 # Copy all vhost in _setup/host to sites-available
+for filename in /vagrant/_setup/host/*.conf; do
+  echo "Copy file $filename and enabling the virtual host..."
 
+  sudo cp "$filename" "$APACHE_SITE/$(basename "$filename")"
+
+  sudo a2ensite "$(basename "$filename")" >> /vagrant/vm_build.log 2>&1
+done
 
 # Enable all site
+sudo service apache2 restart
+
+echo "Virtual host is ready"
